9c67e001f921ef415f879a90c8a6c4c62b23655c
Lam T Duong <lamthachduong@gmail.com>
2022-02-09 22:50:01 -0500

    feat: LeetCode Daily: #560

---
+++ 0560_subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution
+{
+public:
+    // Given an array of integers nums and an integer k, return the total
+    // number of continuous subarrays whose sum equals to k.
+    int subarraySum(std::vector<int>& nums, int k)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+        }
+        return result;
+    }
+};
