4160c150e140cfb9785dd5d06326865f2e347e6d
Lam T Duong <lamthachduong@gmail.com>
2022-02-09 21:32:42 -0500

    fix: Cleaner code.

--- 0013_roman-to-integer.cpp
+++ 0013_roman-to-integer.cpp
@@ -3,30 +3,19 @@
 
 class Solution
 {
+    std::unordered_map<char, int> symbols{{'I', 1},   {'V', 5},   {'X', 10},
+                                          {'L', 50},  {'C', 100}, {'D', 500},
+                                          {'M', 1000}};
 public:
     int romanToInt(std::string s)
     {
-        auto symbols = std::unordered_map<char, int>();
-        symbols['I'] = 1;
-        symbols['V'] = 5;
-        symbols['X'] = 10;
-        symbols['L'] = 50;
-        symbols['C'] = 100;
-        symbols['D'] = 500;
-        symbols['M'] = 1000;
-
-        auto rules = std::unordered_map<char, char>();
-
         int result = 0;
-
-        for (int i = 0; i < s.length(); i++)
+        for (int i = 0; i < (int)s.length(); i++)
         {
-            int value = symbols.at(s[i]);
-
-            if (i != s.length() - 1)
-                if (symbols.at(s[i]) < symbols.at(s[i+1]))
+            int value = symbols[s[i]];
+            if (i != (int)s.length() - 1)
+                if (symbols[s[i]] < symbols[s[i + 1]])
                     value *= -1;
-
             result += value;
         }
         return result;
