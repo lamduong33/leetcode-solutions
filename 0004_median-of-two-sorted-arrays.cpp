41ba76e5113f27294e1ba25d341bccd3cb73e03e
Lam T Duong <lamthachduong@gmail.com>
2022-02-09 20:58:57 -0500

    fix: Formatting and forgot include statement.

--- 0004_median-of-two-sorted-arrays.cpp
+++ 0004_median-of-two-sorted-arrays.cpp
@@ -7,6 +7,10 @@ public:
     // Runtime should be O(log (m+n))
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
     {
-        // Finding the median is O(log(m+n))
+        double median;
+
+
+
+        return median;
     }
 };
