85238ea776bd79a052719c9ccf2c6a5b3c607ef2
Lam T Duong <lamthachduong@gmail.com>
2022-02-09 20:59:16 -0500

    feat: New problem: #5

---
+++ 0005_longest-palindromic-substring.cpp
@@ -0,0 +1,9 @@
+#include <string>
+class Solution
+{
+public:
+    // Given a string s, return the longest palindromic substring in s.
+    std::string longestPalindrome(std::string s)
+    {
+    }
+};
