c1200291b72797649a8fa80577ddb18a70ceeb63
Lam T Duong <lamthachduong@gmail.com>
2022-02-12 10:20:29 -0500

    feat: started working on #567

---
+++ 0567_permutation-in-string.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+class Solution
+{
+public:
+    /**
+     * Given two strings s1 and s2, return true if s2 contains a permutation of
+     * s1, or false otherwise. In other words, return true if one of s1's
+     * permutations is the substring of s2.
+     * */
+    bool checkInclusion(std::string s1, std::string s2)
+    {
+        bool result;
+
+        return result;
+    }
+};
