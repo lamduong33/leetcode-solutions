3b4a693eb5acb0ff76bd9794b55d9f8e265df8be
Lam T Duong <lamthachduong@gmail.com>
2022-02-17 23:36:54 -0500

    fix: Attempt to not have leading 0s. Failed.

--- 0402_remove-k-digits.cpp
+++ 0402_remove-k-digits.cpp
@@ -1,33 +1,40 @@
 #include <string>
+#include <algorithm>
 
 using std::string;
 
-class Solution {
+class Solution
+{
 public:
     string removeKdigits(string num, int k)
     {
         std::string result = "";
 
-        int comp = num.size()-k;
+        int comp = num.size() - k;
         auto lastSmallest = 0;
         bool first0 = true;
 
         for (int i = 0; i < comp; i++)
         {
             auto smallest = num[lastSmallest];
-            auto end = num.size()-comp+i;
-            for (int j = lastSmallest; j < end+1; j++)
+            auto jStart = lastSmallest;
+            for (int j = jStart; j < i + k + 1; j++)
             {
                 if (num[j] < smallest)
                 {
-                    lastSmallest = j+1; // move to next one
+                    if (num[j] != 0)
+                        first0 = false;
+                    lastSmallest = j + 1; // lastSmallest is now the next one.
                     smallest = num[j];
                 }
             }
-            lastSmallest = (i+1 >= lastSmallest) ? i+1 : lastSmallest;
+            lastSmallest =
+                (jStart + 1 >= lastSmallest) ? jStart + 1 : lastSmallest;
+            if ((smallest == '0') && (first0 == true))
+                continue;
             result.push_back(smallest);
         }
-        result = result == "" ? "0" : result; // 0 edge case
+        result = result == "" ? "0" : result; // for 0 edge case.
         return result;
     }
 };
