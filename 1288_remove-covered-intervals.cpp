f31ced4bcaaea5c3c9e8f87f4c87d20d3e29cd7a
Lam T Duong <lamthachduong@gmail.com>
2022-02-20 17:44:24 -0500

    feat: Started #1288

---
+++ 1288_remove-covered-intervals.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using std::vector;
+
+class Solution
+{
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals)
+    {
+        auto result = 0;
+        return result;
+    }
+};
