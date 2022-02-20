3bf8b877a666399ffedd9e66f2ea1bcae0002b4f
Lam T Duong <lamthachduong@gmail.com>
2022-02-19 20:18:05 -0500

    TODO: #1675: Logic is wrong. Need new algorithm.

--- 1675_minimize-deviation-in-array.cpp
+++ 1675_minimize-deviation-in-array.cpp
@@ -5,43 +5,45 @@ using std::vector;
 
 class Solution
 {
+    void checkExtremes(int* SE, int* LO, int* min, int* max, vector<int>& nums)
+    {
+        *SE = 0, *LO = 0, *min = 0, *max = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < *min || *min == 0)
+                *min = nums[i];
+            if (nums[i] > *max)
+                *max = nums[i];
+            if ((nums[i] % 2 == 0) && (nums[i] < *SE) || (*SE == 0))
+                *SE = nums[i];
+            if ((nums[i] % 2 != 0) && (nums[i] > *LO) || (*LO == 0))
+                *LO = nums[i];
+        }
+    }
+
 public:
     int minimumDeviation(vector<int>& nums)
     {
         auto smallestEven = 0, largestOdd = 0, min = 0, max = 0;
-
-        do
+        checkExtremes(&smallestEven, &largestOdd, &min, &max, nums);
+        while (smallestEven != min || largestOdd != max)
         {
             for (int i = 0; i < nums.size(); i++)
             {
-                if (nums[i] < min || min == 0)
-                    min = nums[i];
-                if (nums[i] > max)
-                    max = nums[i];
-                if (nums[i] % 2 == 0)
+                if ((nums[i] % 2 == 0) && (nums[i] / 2 > smallestEven))
                 {
-                    if (nums[i] < smallestEven || smallestEven == 0)
-                        smallestEven = nums[i];
-                    if (nums[i] / 2 > smallestEven)
-                        nums[i] /= 2;
+                    nums[i] /= 2;
                 }
-                else
+                else if ((nums[i] % 2 != 0) && (nums[i] * 2 < largestOdd))
                 {
-                    if (nums[i] > largestOdd || largestOdd == 0)
-                        largestOdd = nums[i];
-                    if (nums[i] / 2 < largestOdd)
-                        nums[i] *= 2;
+                    nums[i] *= 2;
                 }
             }
-            std::cout << std::endl << "min: " << min << " max: " << max;
-            std::cout << " SE: " << smallestEven << " LO: " << largestOdd
-                      << std::endl;
-            ;
+            checkExtremes(&smallestEven, &largestOdd, &min, &max, nums);
         }
-        while (smallestEven != min && largestOdd != max);
-        for (auto e : nums)
-            std::cout << e << ",";
+
         auto result = abs(largestOdd - smallestEven);
+
         return result;
     }
 };
