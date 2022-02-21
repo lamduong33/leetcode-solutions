9f2dc957aa6f3dfa3a7bf3bccfd7d636fd664163
Lam T Duong <lamthachduong@gmail.com>
2022-02-20 21:46:42 -0500

    fix: 16ms and better than 99.57%.

--- 1288_remove-covered-intervals.cpp
+++ 1288_remove-covered-intervals.cpp
@@ -1,12 +1,33 @@
-#include <vector>
-using std::vector;
-
 class Solution
 {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals)
     {
-        auto result = 0;
-        return result;
+        int count = intervals.size();
+        std::sort(intervals.begin(), intervals.end(),
+                  [](const vector<int>& a, const vector<int>& b)
+                  {
+                      if (a[0] == b[0])
+                          return a[1] > b[1];
+                      else
+                          return a[0] < b[0];
+                  });
+        auto c = -1, d = -1;
+
+        for (int i = 0; i < intervals.size(); i++)
+        {
+            int a = intervals[i][0], b = intervals[i][1];
+            if (c <= a && b <= d)
+            {
+                count--;
+            }
+            else
+            {
+                c = intervals[i][0];
+                d = intervals[i][1];
+            }
+        }
+
+        return count;
     }
 };
