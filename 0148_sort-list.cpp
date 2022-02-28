bf422b6522a63016628360d11960b99bc2dd4e9b
Lam T Duong <lamthachduong@gmail.com>
2022-02-28 11:52:52 -0500

    fix: Using heapsort stl.

--- 0148_sort-list.cpp
+++ 0148_sort-list.cpp
@@ -1,31 +1,53 @@
 #include <vector>
 #include <bits/stdc++.h>
+#include <iostream>
 /**
  * Definition for singly-linked list.*/
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
 
 class Solution
 {
 public:
     ListNode* sortList(ListNode* head)
     {
-        // 1) Heap construction
-        auto heap = std::vector<int>{};
+        auto heap = std::vector<ListNode*>{};
         auto pointer = head;
         while (pointer != nullptr)
         {
-            heap.push_back(pointer->val);
+            heap.push_back(pointer);
             pointer = pointer->next;
         }
-        std::make_heap(heap.begin(), heap.end());
 
-        // 2) Maximum deletions
-        return head;
+        // Heap construction
+        std::make_heap(heap.begin(), heap.end(),
+                       [] (const ListNode* node1, ListNode* node2)
+                       {
+                           return node1->val < node2->val;
+                       });
+
+
+        // Heap construction
+        std::sort_heap(heap.begin(), heap.end(),
+                       [] (const ListNode* node1, ListNode* node2)
+                       {
+                           return node1->val < node2->val;
+                       });
+
+        // Construct list from heap
+        auto newList = new ListNode{};
+        auto newHead = newList;
+        for (auto node : heap)
+        {
+            newHead->next = new ListNode{node->val};
+            newHead = newHead->next;
+        }
+        newList = newList->next;
+        return newList;
     }
 };
