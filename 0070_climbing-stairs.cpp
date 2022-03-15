eacf921aa9f97ceda9bf1475cd2511e3015ca50d
Lam T Duong <lamthachduong@gmail.com>
2022-03-15 18:26:20 -0400

    fix: Code is correct but is very very slow.

--- 0070_climbing-stairs.cpp
+++ 0070_climbing-stairs.cpp
@@ -1,16 +1,13 @@
-#include <list>
-#include <vector>
-#include <iostream>
-
 class Solution
 {
 public:
     // Dynamic programming appraoch
     int climbStairs(int n)
     {
-        if (n < 3) return n;
-        std::vector<std::pair<int,int>> paths;
-        std::vector<std::pair<int,int>> newPaths;
+        if (n < 3)
+            return n;
+        std::list<std::pair<int, int>> paths;
+        std::list<std::pair<int, int>> newPaths;
         paths.push_back(std::pair{1, 2});
         paths.push_back(std::pair{0, 2});
 
@@ -20,13 +17,12 @@ public:
             {
                 if (path.second - path.first == 1)
                 {
-                    // FIXME: Memory error: can't write while accessing
-                    newPaths.push_back(std::pair<int, int>{path.second, i});
+                    newPaths.push_back(std::pair<int,int>{path.first, i});
                 }
                 path.first = path.second;
                 path.second = i;
             }
-            paths.insert(paths.end(), newPaths.begin(), newPaths.end());
+            paths.merge(newPaths);
             newPaths.clear();
         }
 
