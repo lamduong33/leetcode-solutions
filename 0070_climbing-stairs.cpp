b31ee5c256a39f5f7d412189193627b95734ccf7
Lam T Duong <lamthachduong@gmail.com>
2022-03-15 20:21:39 -0400

    fix: This is a fibonacci sequence. Ez clap.

--- 0070_climbing-stairs.cpp
+++ 0070_climbing-stairs.cpp
@@ -1,31 +1,16 @@
 class Solution
 {
 public:
-    // Dynamic programming appraoch
     int climbStairs(int n)
     {
-        if (n < 3)
-            return n;
-        std::list<std::pair<int, int>> paths;
-        std::list<std::pair<int, int>> newPaths;
-        paths.push_back(std::pair{1, 2});
-        paths.push_back(std::pair{0, 2});
-
-        for (int i = 3; i <= n; i++)
+        auto result = 1;
+        auto previous = 0;
+        for (int i = 1; i <= n; i++)
         {
-            for (auto& path : paths)
-            {
-                if (path.second - path.first == 1)
-                {
-                    newPaths.push_back(std::pair<int,int>{path.first, i});
-                }
-                path.first = path.second;
-                path.second = i;
-            }
-            paths.merge(newPaths);
-            newPaths.clear();
+            auto temp = result;
+            result += previous;
+            previous = temp;
         }
-
-        return (int)paths.size();
+        return result;
     }
 };
