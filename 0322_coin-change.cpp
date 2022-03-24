eb036256452dfff2bf47eb08fda78f71196760fe
Lam T Duong <lamthachduong@gmail.com>
2022-03-24 00:14:58 -0400

    TODO: Still working on coin change

--- 0322_coin-change.cpp
+++ 0322_coin-change.cpp
@@ -1,16 +1,18 @@
+#include <algorithm>
 #include <vector>
 using std::vector;
 
 class Solution
 {
 public:
-
     int coinChange(vector<int>& coins, int amount)
     {
         if (amount == 0) return 0;
         auto result = -1;
+        std::sort(coins.begin(), coins.end());
+
         auto largestCoin = coins[coins.size()-1];
-        for (auto i = 0; i < coins.size(); i++)
+        for (auto i = 0; i < (int)coins.size(); i++)
         {
             if (amount == coins[i])
                 return 1;
@@ -24,7 +26,9 @@ public:
                 break;
             }
         }
-        result = coinChange(coins, amount-largestCoin)+1;
+        vector<int>{2,2};
+        result = coinChange(coins, amount-largestCoin);
+        if (result != -1) result++;
         return result;
     }
 };
