ebcc03734bc78d54a65d210412f0f12c7a0b5281
Lam T Duong <lamthachduong@gmail.com>
2022-03-29 21:56:27 -0400

    fix: Two pointers method. Faster than 88%

--- 0081_search-in-rotated-sorted-array-ii.cpp
+++ 0081_search-in-rotated-sorted-array-ii.cpp
@@ -6,10 +6,13 @@ public:
     bool search(std::vector<int>& nums, int target)
     {
         bool result = false;
-        auto first = true;
-        for (auto num : nums)
+        auto fp = nums.begin();
+        for (auto sp = nums.begin(); sp != nums.end(); sp++)
         {
-
+            if (*sp == target || *fp == target)
+                return true;
+            if (fp < nums.begin())
+                fp += 2; // Increment fast pointer twice
         }
         return result;
     }
